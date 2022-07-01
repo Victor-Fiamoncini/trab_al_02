2daa14b8a4edd13a0bbedc1392738922f04582b6
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-06-30 23:08:47 -0300

    chore: add base pipenv setup & add pytest

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from typing import List
+
+def main():
+    vector = [[2,1], [1,2]]
+
+    print(get_norma(vector))
+
+def get_norma(vector: List[List[int]]):
+    pass
+
+if __name__ == "__main__":
+    main()
